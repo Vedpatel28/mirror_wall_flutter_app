1e5f07813ff9dd794a640c92b3b4f1955d81000b
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-07-01 10:32:11 +0530

    | VP | 30/06/23 | Connectivity Check |

--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: camel_case_types
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -5,6 +7,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mirror_wall_flutter_app/modals/list_search_engine.dart';
 import 'package:mirror_wall_flutter_app/utils/routes_utils.dart';
 
+// ignore: must_be_immutable
 class home_page extends StatelessWidget {
   home_page({super.key});
 
@@ -12,8 +15,6 @@ class home_page extends StatelessWidget {
 
   String initialUrlRequest =
       'https://pub.dev/packages/flutter_inappwebview/example';
-
-  double _progress = 0;
 
   InAppWebViewController? inAppWebViewController;
 
@@ -32,79 +33,322 @@ class home_page extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            SizedBox(
-              height: s.height * 0.45,
-              child: GridView.builder(
-                itemCount: AllWebside.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 3,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemBuilder: (context, index) => GestureDetector(
-                  onTap: () {
-                    Navigator.of(context)
-                        .pushNamed(AllRoutes.WebPage, arguments: index);
-                  },
+      body: StreamBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            ConnectivityResult result = snapshot.hasData as ConnectivityResult;
+            switch (result) {
+              case ConnectivityResult.mobile:
+                return Padding(
+                  padding: const EdgeInsets.all(12),
                   child: Column(
                     children: [
-                      Expanded(
-                        flex: 5,
-                        child: Container(
-                          height: double.infinity,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            // color: Colors.grey,
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.black54,
-                                blurRadius: 3,
-                                spreadRadius: 2,
-                                offset: Offset(0.2, 0.1),
-                                blurStyle: BlurStyle.outer,
-                              ),
-                            ],
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 2,
-                            ),
-                            image: DecorationImage(
-                              image: NetworkImage(AllWebside[index].logo),
-                            ),
+                      SizedBox(
+                        height: s.height * 0.45,
+                        child: GridView.builder(
+                          itemCount: AllWebside.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 3 / 3,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
                           ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          height: double.infinity,
-                          width: double.infinity,
-                          decoration: const BoxDecoration(),
-                          alignment: const Alignment(0, 0),
-                          child: Text(
-                            AllWebside[index].name,
-                            style: GoogleFonts.dmSerifText(
-                              textStyle: const TextStyle(
-                                fontSize: 15,
-                                color: Colors.black87,
-                                fontWeight: FontWeight.bold,
-                              ),
+                          itemBuilder: (context, index) => GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pushNamed(AllRoutes.WebPage,
+                                  arguments: index);
+                            },
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  flex: 5,
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      // color: Colors.grey,
+                                      boxShadow: const [
+                                        BoxShadow(
+                                          color: Colors.black54,
+                                          blurRadius: 3,
+                                          spreadRadius: 2,
+                                          offset: Offset(0.2, 0.1),
+                                          blurStyle: BlurStyle.outer,
+                                        ),
+                                      ],
+                                      border: Border.all(
+                                        color: Colors.black,
+                                        width: 2,
+                                      ),
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                            AllWebside[index].logo),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    decoration: const BoxDecoration(),
+                                    alignment: const Alignment(0, 0),
+                                    child: Text(
+                                      AllWebside[index].name,
+                                      style: GoogleFonts.dmSerifText(
+                                        textStyle: const TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
                       ),
                     ],
                   ),
-                ),
+                );
+              case ConnectivityResult.wifi:
+                return Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: s.height * 0.45,
+                        child: GridView.builder(
+                          itemCount: AllWebside.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 3 / 3,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                          ),
+                          itemBuilder: (context, index) => GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pushNamed(AllRoutes.WebPage,
+                                  arguments: index);
+                            },
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  flex: 5,
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      // color: Colors.grey,
+                                      boxShadow: const [
+                                        BoxShadow(
+                                          color: Colors.black54,
+                                          blurRadius: 3,
+                                          spreadRadius: 2,
+                                          offset: Offset(0.2, 0.1),
+                                          blurStyle: BlurStyle.outer,
+                                        ),
+                                      ],
+                                      border: Border.all(
+                                        color: Colors.black,
+                                        width: 2,
+                                      ),
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                            AllWebside[index].logo),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    decoration: const BoxDecoration(),
+                                    alignment: const Alignment(0, 0),
+                                    child: Text(
+                                      AllWebside[index].name,
+                                      style: GoogleFonts.dmSerifText(
+                                        textStyle: const TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              case ConnectivityResult.ethernet:
+                return Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: s.height * 0.45,
+                        child: GridView.builder(
+                          itemCount: AllWebside.length,
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 3 / 3,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                          ),
+                          itemBuilder: (context, index) => GestureDetector(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .pushNamed(AllRoutes.WebPage, arguments: index);
+                            },
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  flex: 5,
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      // color: Colors.grey,
+                                      boxShadow: const [
+                                        BoxShadow(
+                                          color: Colors.black54,
+                                          blurRadius: 3,
+                                          spreadRadius: 2,
+                                          offset: Offset(0.2, 0.1),
+                                          blurStyle: BlurStyle.outer,
+                                        ),
+                                      ],
+                                      border: Border.all(
+                                        color: Colors.black,
+                                        width: 2,
+                                      ),
+                                      image: DecorationImage(
+                                        image: NetworkImage(AllWebside[index].logo),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    decoration: const BoxDecoration(),
+                                    alignment: const Alignment(0, 0),
+                                    child: Text(
+                                      AllWebside[index].name,
+                                      style: GoogleFonts.dmSerifText(
+                                        textStyle: const TextStyle(
+                                          fontSize: 15,
+                                          color: Colors.black87,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              default:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+            }
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: s.height * 0.45,
+                    child: GridView.builder(
+                      itemCount: AllWebside.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 3 / 3,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                      ),
+                      itemBuilder: (context, index) => GestureDetector(
+                        onTap: () {
+                          Navigator.of(context)
+                              .pushNamed(AllRoutes.WebPage, arguments: index);
+                        },
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                height: double.infinity,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  // color: Colors.grey,
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Colors.black54,
+                                      blurRadius: 3,
+                                      spreadRadius: 2,
+                                      offset: Offset(0.2, 0.1),
+                                      blurStyle: BlurStyle.outer,
+                                    ),
+                                  ],
+                                  border: Border.all(
+                                    color: Colors.black,
+                                    width: 2,
+                                  ),
+                                  image: DecorationImage(
+                                    image: NetworkImage(AllWebside[index].logo),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                height: double.infinity,
+                                width: double.infinity,
+                                decoration: const BoxDecoration(),
+                                alignment: const Alignment(0, 0),
+                                child: Text(
+                                  AllWebside[index].name,
+                                  style: GoogleFonts.dmSerifText(
+                                    textStyle: const TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black87,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
+            );
+          }
+        },
       ),
     );
   }
